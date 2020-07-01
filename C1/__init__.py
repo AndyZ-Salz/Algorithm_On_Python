6b934efccfb1e227c01d07a9ff3fe6ac0bcdbf49
AndyZang-Salz <kaiyuezang@gmail.com>
2020-07-01 11:12:06 +0800

    ignore IDEA

---
+++ C1/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : __init__.py
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : 
+---------------------------------------
+"""
+
+# History:
+# 2020/6/30: Create
+
+
+if __name__ == '__main__':
+    pass
