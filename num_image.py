353b849dacdd1ea6ebb98109a0d33250a1cde0fb
AndyZang-Salz <kaiyuezang@gmail.com>
2021-07-13 14:55:03 +0800

    some test things

---
+++ num_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : num_image
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : 
+---------------------------------------
+"""
+
+
+# History:
+# 2021/3/10: Create
+
+
+image1 = (
+0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF,
+)
---
+++ ttt.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : ttt
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : 
+---------------------------------------
+"""
+
+
+# History:
+# 2021/3/10: Create
+
+
+import num_image
+
+print(num_image.image1)
